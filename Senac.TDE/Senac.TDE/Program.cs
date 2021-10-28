3edcd41845c76e7dfd12b11b74b8c7c832fa4e12
Your Name <you@example.com>
2021-10-28 19:37:39 -0300

    Iniciando Projeto

---
+++ Senac.TDE/Senac.TDE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senac.TDE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
